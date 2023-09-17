7cab43a13e8baad59052443a141566a03641048d
Rurusachi <39674427+Rurusachi@users.noreply.github.com>
2023-09-17 20:13:21 +0200

    Pack script for workflow

---
+++ .github/pack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+fvtt package workon "loghorizontrpg-compendium" --type "Module"
+
+for p in packs/*
+do
+	fvtt package pack "${p#packs/}" --in "${p}/_source" --out "${p}"
+done
+
+echo Packing finished
